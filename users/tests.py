89346a4d20b18ff42486c27cf84df673bcc8b65d
Ferdinandy Chagas <ferdinandsc@yahoo.com.br>
2024-03-21 23:56:05 -0300

    update test settings

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    """ Test Case example """
+
+    def setUp(self) -> None:
+        self.name = "user_test_case"
+
+    def test_user(self):
+        """ test example """
+        self.assertEqual(1, 1)
